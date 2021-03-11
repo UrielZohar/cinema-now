bab5729c326779f35f2a3d66f4060d235fbd33e3
Uriel Zohar <urielfed@gmail.com>
2021-03-11 14:26:15 +0200

    app use

--- cinema-now-server/routes/index.js
+++ cinema-now-server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get("/*", function(req, res, next) {
-  res.status(200).sendFile(path.join(__dirname, 'dist', 'cinema-now-client', 'index.html'));
+  res.status(200).sendFile(path.join(__dirname, '..', 'dist', 'cinema-now-client', 'index.html'));
 });
 
 module.exports = router;
